1dafb9b28a32f3f971e5feec7d698de4bae70357
j49_c <j49_c@github.com>
2026-04-05 23:25:04 +0300

    Fix: DNS Fix getaddrinfo family collision

--- dns_fix.py
+++ dns_fix.py
@@ -4,10 +4,9 @@ def patch_dns():
     print("[DNS FIX] Patching socket.getaddrinfo to force IPv4 resolution...")
     _orig_getaddrinfo = socket.getaddrinfo
 
-    def new_getaddrinfo(*args, **kwargs):
-        if len(args) >= 1:
-            kwargs['family'] = socket.AF_INET
-        return _orig_getaddrinfo(*args, **kwargs)
+    def new_getaddrinfo(host, port, family=0, type=0, proto=0, flags=0):
+        # Force AF_INET (IPv4) resolution
+        return _orig_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
 
     socket.getaddrinfo = new_getaddrinfo
 
